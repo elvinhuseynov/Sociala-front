2a57a040d3bfc46457f743d406cd1a5a9c9b5fff
elvinhuseynov <huseynovelvin2018@gmail.com>
2021-09-19 12:22:52 +0400

    initial commit

--- components/counter/counterSlice.ts
+++ components/counter/counterSlice.ts
@@ -69,6 +69,7 @@ export const { increment, decrement, incrementByAmount } = counterSlice.actions;
 export const selectCount = (state: AppState) => state.counter.value;
 
 // We can also write thunks by hand, which may contain both sync and async logic.
+
 // Here's an example of conditionally dispatching actions based on current state.
 export const incrementIfOdd =
   (amount: number): AppThunk =>
