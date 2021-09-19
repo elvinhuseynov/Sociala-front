aea6486277e5bbc834f90d34b44e85f438965957
elvinhuseynov <huseynovelvin2018@gmail.com>
2021-09-19 12:09:47 +0400

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import "../styles/globals.css";
+
+import { Provider } from "react-redux";
+import type { AppProps } from "next/app";
+
+import store from "../app/store";
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <Provider store={store}>
+    <Component {...pageProps} />
+  </Provider>
+);
+
+export default App;
